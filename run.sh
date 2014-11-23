b74a83f08901987a3fe2d5148544b901c46d4480
bmartel <brandonmartel@gmail.com>
2014-11-23 15:47:17 -0600

    Modify run command to detect the environment.

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@
 
 PYTHON_V=$(which python)
 
-if [[ "$(uname -s | tr '[:upper:]' '[:lower:]')" -eq "darwin" ]]; then
+if [ "$(uname)" == "Darwin" ]; then
+    # Run python under 32bit for OSX
     arch -i386 $PYTHON_V serve.py
-else
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Run python as is.
     $PYTHON_V serve.py
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Optional handling under windows git bash.
 fi
