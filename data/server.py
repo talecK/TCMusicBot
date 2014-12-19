32202cf24910f33793f87c99a64c83800611c62d
bmartel <brandonmartel@gmail.com>
2014-12-19 15:51:48 -0600

    modifying the server stat tracking

--- data/server.py
+++ data/server.py
@@ -12,15 +12,8 @@ class ServerDataAccess(object):
         self.storage = MongoConnection(db='tc_music', collection='server_stats')
 
     def create_server_statistics(self):
-        # Remove any previous server stats from previous sessions
-        self.storage.use_collection('server_stats').remove()
-
         # Rebuild the server stats entry
-        stats_id = self.storage.use_collection('server_stats').insert({
-            'currently_playing': {},
-            'status': 'polling',
-            'volume': '50'
-        })
+        stats_id = self.set_currently_playing()
 
         print "Setting server id " +  str(stats_id)
 
@@ -46,15 +39,16 @@ class ServerDataAccess(object):
             return self.storage.use_collection('server_stats').update({}, {'$set':{'currently_playing': extract_song_data(song), 'status': 'playing'}},upsert=False)
         else:
             print "Server polling..."
-            return self.storage.use_collection('server_stats').update({}, {'$set':{'currently_playing': {}, 'status': 'polling', 'volume': '50'}},upsert=False)
-
-        return False
+            return self.storage.use_collection('server_stats').update({}, {'$set':{'currently_playing': {}, 'status': 'polling', 'volume': '50'}},upsert=True)
 
     def get_total_songs_played(self):
         return self.storage.use_collection('server_stats').find_one({'total_songs_played': True})
 
     def set_total_songs_played(self, total_songs):
         return self.storage.use_collection('server_stats').update({}, {'$set':{'total_songs_played': total_songs}},upsert=False)
+
+    def increment_total_songs_played(self):
+        return self.storage.use_collection('server_stats').update({}, {'$inc':{'total_songs_played': 1}},upsert=False)
 
     def get_server_status(self):
         return self.storage.use_collection('server_stats').find_one({'status': True})
